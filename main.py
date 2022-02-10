242e0ee5416d051c248d14201746ea2231ce00e0
Данил Низамов <danilnizamov@MacBook-Air-Danil.local>
2022-02-10 03:46:12 +0300

    added initial funcs

--- main.py
+++ main.py
@@ -1,2 +1,38 @@
-print("PRIVET DIMAS")
-print("darova")
+import telebot
+
+
+TOKEN = "5245318135:AAGg9yEYwhea8g1iCu-WYaZPMcYTtsU0SS4"
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(content_types=["text"])
+def start(message):
+    """Checks if user registered or not
+    if not, calls register function
+    if yes, call main_menu function
+    """
+    main_menu(message)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    """ The main callback worker,
+    checks call_back from every function
+    """
+    pass
+
+
+def main_menu(message):
+    keyboard = telebot.types.ReplyKeyboardMarkup()
+    key_setup = telebot.types.KeyboardButton(
+        text="Настроить скрипт",
+    )
+    key_about = telebot.types.KeyboardButton(
+        text="Как пользоваться?",
+    )
+    keyboard.add(key_setup)
+    keyboard.add(key_about)
+    bot.send_message(message.chat.id, reply_markup=keyboard, text="Выберите опцию")
+
+bot.polling(none_stop=True)
